7887a8795c1a1c70d2e650f897e25348805f307f
Arihant Tripathy <arihanttr@live.com>
2024-03-20 00:11:09 +0530

    Add CSX page and Data Structures and Algorithms page

---
+++ src/app/courses/Data-Structures-and-Algorithms/page.tsx
@@ -0,0 +1,176 @@
+'use client';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+const NavBar = () => {
+  const router = useRouter();
+
+  return (
+    <nav className="w-full bg-white py-3 text-right">
+      <button onClick={() => router.back()} className="text-indigo-600">
+        Back
+      </button>
+    </nav>
+  );
+};
+
+const DSAPage = () => {
+  const [formData, setformData] = useState({
+    quiz1: '',
+    midSemTheory: '',
+    midSemLab: '',
+    quiz2: '',
+    endSemTheory: '',
+    endSemLab: '',
+    labs: Array(8).fill(''),
+    assignments: Array(5).fill(''),
+  });
+
+  const handleInputChange = (e, index = null) => {
+    if (index !== null) {
+      if (e.target.name.startsWith('lab')) {
+        const updatedLabs = [...formData.labs];
+        updatedLabs[index] = e.target.value;
+        setformData({ ...formData, labs: updatedLabs });
+      } else if (e.target.name.startsWith('assignment')) {
+        const updatedAssignments = [...formData.assignments];
+        updatedAssignments[index] = e.target.value;
+        setformData({ ...formData, assignments: updatedAssignments });
+      }
+    } else {
+      setformData({ ...formData, [e.target.name]: e.target.value });
+    }
+  };
+
+  const calculateMarks = () => {
+    // Set the default value of each to 0
+    for (let key in formData) {
+      if (Array.isArray(formData[key])) {
+        formData[key] = formData[key].map((value) => (value === '' ? 0 : value));
+      } else {
+        formData[key] = formData[key] === '' ? 0 : formData[key];
+      }
+    }
+    // Calculate the total marks
+    let assignments = (formData.assignments[0] / 200 + formData.assignments[1] / 200 + formData.assignments[2] / 200 + formData.assignments[3] / 200 + formData.assignments[4] / 200) * 2;
+    let lab = (formData.labs[0] / 200 + formData.labs[1] / 200 + formData.labs[2] / 200 + formData.labs[3] / 200 + formData.labs[4] / 200 + formData.labs[5] / 200 + formData.labs[6] / 200 + formData.labs[7] / 200) * 3.75;
+    let quiz = ((parseInt(formData.quiz1) / 100) + (parseInt(formData.quiz2) / 100)) * 7.5;
+    let mid = ((parseInt(formData.midSemTheory) / 140) + (parseInt(formData.midSemLab) / 450)) * 7.5;
+    let end = ((parseInt(formData.endSemTheory) / 140) + (parseInt(formData.endSemLab) / 450)) * 7.5;
+    let final = assignments + lab + quiz + mid + end;
+    console.log(quiz, mid, end, assignments, lab, final);
+    alert(`Total marks: ` + parseFloat(`${final}`).toFixed(2));
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center text-black">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <nav className="w-full bg-white p-4 text-right">
+          <button onClick={() => history.back()} className="text-indigo-600">
+            Back
+          </button>
+        </nav>
+        <h2 className="text-5xl font-bold mb-6 text-center">DSA</h2>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">Quiz 1 (out of 100)</h3>
+          <input
+            type="number"
+            name="quiz1"
+            value={formData.quiz1}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">Mid Sem Theory (out of 140)</h3>
+          <input
+            type="number"
+            name="midSemTheory"
+            value={formData.midSemTheory}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">Mid Sem Lab (out of 450)</h3>
+          <input
+            type="number"
+            name="midSemLab"
+            value={formData.midSemLab}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          />
+        </div>
+        <div className="mb-6">
+          <h3 className="text-lg font-semibold mb-2">Quiz 2</h3>
+          <input
+            type="number"
+            name="quiz2"
+            value={formData.quiz2}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">End Sem Theory</h3>
+          <input
+            type="number"
+            name="endSemTheory"
+            value={formData.endSemTheory}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">End Sem Lab</h3>
+          <input
+            type="number"
+            name="endSemLab"
+            value={formData.endSemLab}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">Labs (out of 200 each)</h3>
+          <div className="grid grid-cols-4 gap-4">
+            {formData.labs.map((value, index) => (
+              <input
+                key={index}
+                type="number"
+                name={`lab${index + 1} `}
+                value={value}
+                onChange={(e) => handleInputChange(e, index)}
+                className="border border-gray-300 rounded-md p-2"
+                placeholder={`Lab ${index + 1} `}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">Assignments (out of 200 each)</h3>
+          <div className="grid grid-cols-5 gap-4">
+            {formData.assignments.map((value, index) => (
+              <input
+                key={index}
+                type="number"
+                name={`assignment${index + 1} `}
+                value={value}
+                onChange={(e) => handleInputChange(e, index)}
+                className="border border-gray-300 rounded-md p-2"
+                placeholder={`Assignment ${index + 1} `}
+              />
+            ))}
+          </div>
+        </div>
+        <button
+          onClick={calculateMarks}
+          className="bg-indigo-600 text-white font-semibold py-2 px-4 rounded-md hover:bg-indigo-700 transition-colors duration-300"
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DSAPage;
---
+++ src/app/csx/page.tsx
@@ -0,0 +1,123 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import courses from './courses.json';
+
+interface Course {
+  name: string;
+  semester: string;
+  color: string;
+}
+
+const colorClasses = {
+  red: 'bg-red-500',
+  blue: 'bg-blue-500',
+  green: 'bg-green-500',
+  yellow: 'bg-yellow-500',
+  indigo: 'bg-indigo-500',
+  purple: 'bg-purple-500',
+  pink: 'bg-pink-500',
+  gray: 'bg-gray-500',
+  teal: 'bg-teal-500',
+  orange: 'bg-orange-500',
+  sky: 'bg-sky-500',
+  lime: 'bg-lime-500',
+  emerald: 'bg-emerald-500',
+  amber: 'bg-amber-500',
+  rose: 'bg-rose-500'
+};
+
+const getCurrentSemester = (): string => {
+  const now = new Date();
+  const month = now.getMonth();
+  const year = now.getFullYear();
+
+  let sem;
+  if (month >= 1 && month <= 6) {
+    sem = 'Sem 1-2';
+  } else {
+    sem = 'Sem 2-1';
+  }
+  return `${sem} ${year}`;
+};
+
+const CSXPage = () => {
+  const [currentSemester, setCurrentSemester] = useState<string>(getCurrentSemester());
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const [semesterCourses, setSemesterCourses] = useState<Course[]>([]);
+
+  useEffect(() => {
+    if (courses) {
+      const filteredCourses = courses.filter((course: Course) => course.semester === currentSemester);
+      setSemesterCourses(filteredCourses);
+    }
+  }, [currentSemester, courses]);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const selectSemester = (semester: string) => {
+    setCurrentSemester(semester);
+    setShowDropdown(false);
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-indigo-600 py-4">
+        <div className="container mx-auto flex justify-center relative">
+          <div className="flex items-center text-3xl lg:text-5xl font-semibold cursor-pointer text-white" onClick={toggleDropdown}>
+            {currentSemester}
+            <div className="ml-2 cursor-pointer" onClick={toggleDropdown}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-8 w-8">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+              </svg>
+            </div>
+          </div>
+
+          {showDropdown && (
+            <div className="absolute top-16 bg-white rounded-md shadow-lg z-10 text-black text-center">
+              <div className="py-2 px-6 text-3xl hover:bg-gray-100 cursor-pointer" onClick={() => selectSemester('Sem 1-2 2024')}>
+                Sem 1-2
+              </div>
+              <div className="py-2 px-6 text-3xl hover:bg-gray-100 cursor-pointer" onClick={() => selectSemester('Sem 2-1 2024')}>
+                Sem 2-1
+              </div>
+              <div className="py-2 px-6 text-3xl hover:bg-gray-100 cursor-pointer" onClick={() => selectSemester('Sem 2-2 2025')}>
+                Sem 2-2
+              </div>
+              <div className="py-2 px-6 text-3xl hover:bg-gray-100 cursor-pointer" onClick={() => selectSemester('Sem 3-1 2025')}>
+                Sem 3-1
+              </div>
+              <div className="py-2 px-6 text-3xl hover:bg-gray-100 cursor-pointer" onClick={() => selectSemester('Sem 3-2 2026')}>
+                Sem 3-2
+              </div>
+              <div className="py-2 px-6 text-3xl hover:bg-gray-100 cursor-pointer" onClick={() => selectSemester('Sem 4-1 2026')}>
+                Sem 4-1
+              </div>
+              <div className="py-2 px-6 text-3xl hover:bg-gray-100 cursor-pointer" onClick={() => selectSemester('Sem 4-2 2027')}>
+                Sem 4-2
+              </div>
+            </div>
+          )}
+        </div>
+      </header>
+      <main className="container mx-auto py-8">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-center">
+          {semesterCourses.map((course) => (
+            <Link href={"courses/" + course.name.replace(/\s/g, '-')} key={course.name}>
+              <div
+                className={`bg-${course.color}-500 rounded-lg shadow-md p-6 cursor-pointer flex justify-center items-center hover:scale-105 hover:drop-shadow-lg transition duration-200 h-full`}
+              >
+                <h3 className="text-2xl font-semibold">{course.name}</h3>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default CSXPage;
