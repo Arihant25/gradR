ecbaf58cce8af9bac3443e6b1ed03f65324909f7
Arihant Tripathy <arihanttr@live.com>
2024-03-21 07:36:44 +0530

    Disable typing errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const config = {
+    typescript: {
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        ignoreBuildErrors: true,
+    },
+};
+
+export default { ...config, ...nextConfig };
